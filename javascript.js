08c46ac78da357265cd3a18d44767be0e9a3e162
Ryan Nkansah <ryannkan@gmail.com>
2023-08-01 20:43:42 -0400

    Finished entire project

--- javascript.js
+++ javascript.js
@@ -1,7 +1,39 @@
 container = document.querySelector('.container')
+boxgen = document.querySelector('#myButton').onclick = function(){
+    let userRequestedCount = document.querySelector("#myText").value;
+    if (userRequestedCount > 100) {
+        alert("please choose a smaller number than 100")
+        return
+    }
+
+    randColor = () => {
+        randNum1 = Math.floor(Math.random() * 256)
+        randNum2 = Math.floor(Math.random() * 256)
+        randNum3 = Math.floor(Math.random() * 256)
+        return 'rgb(' + randNum1 + ',' + randNum2 + ',' + randNum3 + ')';
+        
+    }
+    
+    
+  
 
 
-for (let i = 0; i <= 32; i++) {
-    divGrid = document.createElement('div')
-    container.append(divGrid)
+    for (let i = 0; i <= userRequestedCount * userRequestedCount; i++) {
+        divGrid = document.createElement('div')
+        divGrid.classList.add('grid-element')
+        divGrid.addEventListener('mouseover', function(e){
+            e.target.style.backgroundColor = randColor()
+        })
+        container.append(divGrid) 
+        
+    }
 }
+
+
+
+
+
+
+
+
+
